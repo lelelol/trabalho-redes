4656600a796d89bdf2887d97ee207bcdf4ee756b
Gabriella <gabriellamargarida18@gmail.com>
2025-08-16 18:40:21 -0300

    adiciona tratamento de conexão e desconexão no servidor WebSocket

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 const rooms = new Map();
+
+wss.on('connection', ws => {
+    console.log("conectou!");
+
+    ws.on('close', () => {
+        console.log('desconectado');
+    });
+});
